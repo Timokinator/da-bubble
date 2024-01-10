441f3763adce0442d334a265bc88984b67465ac8
Timo Kempf <124527843+Timokinator@users.noreply.github.com>
2024-01-10 22:01:06 +0100

    first commit

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideClientHydration(), importProvidersFrom(provideFirebaseApp(() => initializeApp({ "projectId": "da-bubble-9f879", "appId": "1:872329683690:web:21114e02f86b180bd52d93", "storageBucket": "da-bubble-9f879.appspot.com", "apiKey": "AIzaSyDmu3sXXJKQu_H4grv8B-H8i5Bx3jbFmQc", "authDomain": "da-bubble-9f879.firebaseapp.com", "messagingSenderId": "872329683690" }))), importProvidersFrom(provideAuth(() => getAuth())), importProvidersFrom(provideFirestore(() => getFirestore())), provideAnimations()]
 };
