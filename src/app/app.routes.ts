bfbaadb1b636c4d7ef4a000c06c095d76b0256c6
Benjamin Tietz <benjamin0tietz@gmail.com>
2024-01-13 12:10:22 +0100

    implemented pre-.login componentes /environment folder & auth guard // updated app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,24 @@
 import { Routes } from '@angular/router';
+//pre-login components
+import { LogInComponent } from './pre-login-components/log-in/log-in.component';
+import { SignUpComponent } from './pre-login-components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './pre-login-components/forgot-password/forgot-password.component';
+import { ResetPasswordComponent } from './pre-login-components/reset-password/reset-password.component';
+import { ChooseAvatarComponent } from './pre-login-components/choose-avatar/choose-avatar.component';
+
+//post-login components
+
+//guards
+import { authGuard } from './services/auth.guard';
+//services
+
 
 export const routes: Routes = [
-
-    
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'signup', component: SignUpComponent },
+    { path: 'login', component: LogInComponent },
+    { path: 'choose-avatar', component: ChooseAvatarComponent },
+    { path: 'forgot-password', component: ForgotPasswordComponent },
+    { path: 'reset-password', component: ResetPasswordComponent },
+    { path: '**', component: LogInComponent}                            //catch all other routes and redirect to login
 ];
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDmu3sXXJKQu_H4grv8B-H8i5Bx3jbFmQc",
+        authDomain: "da-bubble-9f879.firebaseapp.com",
+        projectId: "da-bubble-9f879",
+        storageBucket: "da-bubble-9f879.appspot.com",
+        messagingSenderId: "872329683690",
+        appId: "1:872329683690:web:21114e02f86b180bd52d93"
+    }
+};
